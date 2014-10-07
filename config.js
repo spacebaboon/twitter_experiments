86cb0640c2d4778f9d973fdadabf208def13789f
Ben Avery <spacebaboon@gmail.com>
2014-10-07 22:16:56 +0100

    node app lists random tweets with account names for 30s

---
+++ config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var config = {};
+
+config.consumerKey = 'ENTER YOURS HERE';
+config.consumerSecret = 'ENTER YOURS HERE';
+config.accessToken = 'ENTER YOURS HERE';
+config.accessTokenSecret =  'ENTER YOURS HERE';
+
+module.exports = config;
